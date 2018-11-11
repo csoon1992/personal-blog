7dd11f7d38be89067ffe2cf63d6ef79a8dbe7148
Cristina Soler <csoon1992@gmail.com>
2018-11-11 14:19:56 +0100

    Gets the post image in the query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -56,6 +56,7 @@ export const pageQuery = graphql`
           frontmatter {
             title
             tags
+            image
             contentType
             day: date(formatString: "DD")
             month: date(formatString: "MMMM")
