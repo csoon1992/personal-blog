6da1cd34e6e6a6c743fc8e1d969033d45125eda4
Cristina Soler <csoon1992@gmail.com>
2019-01-12 14:15:31 +0100

    Install slugify package and add name attribute to the h2 elements

--- src/components/Blog/Md/Subtitle.js
+++ src/components/Blog/Md/Subtitle.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import slugify from 'slugify';
 
 const Subtitle = ({children}) => (
-    <h2 className="mdc subtitle mt-4 mb-2">
+    <h2 className="mdc subtitle mt-4 mb-2" name={slugify(children[0], {lower: true})}>
         {children}
     </h2>
 );
