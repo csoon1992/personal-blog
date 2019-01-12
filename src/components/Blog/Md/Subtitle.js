be460091b98430b8fd8bc61e7e6d94133df7d1a0
Cristina Soler <csoon1992@gmail.com>
2019-01-12 14:15:31 +0100

    Revert "Install slugify package and add name attribute to the h2 elements"
    This reverts commit 6da1cd34e6e6a6c743fc8e1d969033d45125eda4.

--- src/components/Blog/Md/Subtitle.js
+++ src/components/Blog/Md/Subtitle.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import slugify from 'slugify';
 
 const Subtitle = ({children}) => (
-    <h2 className="mdc subtitle mt-4 mb-2" name={slugify(children[0], {lower: true})}>
+    <h2 className="mdc subtitle mt-4 mb-2">
         {children}
     </h2>
 );
