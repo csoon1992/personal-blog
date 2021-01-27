653dde5e34f88f085e45a26a2a12254b881513c7
Cristina Soler <csoon1992@gmail.com>
2021-01-27 10:58:10 +0100

    Fix dependencies config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,46 @@
 module.exports = {
   pathPrefix: `/files`,
   siteMetadata: {
-    title: 'Cristina Soler',
-    subtitle: 'Desarrolladora web',
+    title: "Cristina Soler",
+    subtitle: "Desarrolladora web",
     technologies: [
       {
-        label: 'Gatsby',
-        icon: 'techs/gatsby.svg',
-        link: 'https://www.gatsbyjs.org'
+        label: "Gatsby",
+        icon: "techs/gatsby.svg",
+        link: "https://www.gatsbyjs.org",
       },
       {
-        label: 'Netlify',
-        icon: 'techs/netlify.svg',
-        link: 'https://www.netlify.com/'
+        label: "Netlify",
+        icon: "techs/netlify.svg",
+        link: "https://www.netlify.com/",
       },
       {
-        label: 'React.js',
-        icon: 'techs/reactjs.svg',
-        link: 'https://reactjs.org/'
+        label: "React.js",
+        icon: "techs/reactjs.svg",
+        link: "https://reactjs.org/",
       },
       {
-        label: 'GraphQL',
-        icon: 'techs/graphql.svg',
-        link: 'https://graphql.org/'
-      }
+        label: "GraphQL",
+        icon: "techs/graphql.svg",
+        link: "https://graphql.org/",
+      },
     ],
-    disqus: 'cristinasoler',// put your disqus ID here
+    disqus: "cristinasoler", // put your disqus ID here
     author: {
-      name: 'Cristina Soler',
-      email: 'csoon1992@gmail.com',
-      github: 'https://github.com/csoon1992'
+      name: "Cristina Soler",
+      email: "csoon1992@gmail.com",
+      github: "https://github.com/csoon1992",
     },
     blogMenu: [
       {
-        label: 'Artículos',
-        link: '/blog/'
+        label: "Artículos",
+        link: "/blog/",
       },
       {
-        label: 'Sobre mí',
-        link: '/about/'
-      }
-    ]
+        label: "Sobre mí",
+        link: "/about/",
+      },
+    ],
   },
   plugins: [
     {
@@ -49,27 +49,27 @@ module.exports = {
         trackingId: "UA-135936584-1",
       },
     },
-    'gatsby-plugin-remove-serviceworker',
+    "gatsby-plugin-remove-serviceworker",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages'
-      }
+        name: "pages",
+      },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
             resolve: `gatsby-remark-autolink-headers`,
             options: {
-              icon: false
-            }
+              icon: false,
+            },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-component',
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-component",
           {
             resolve: `gatsby-remark-images`,
             options: {
@@ -78,38 +78,37 @@ module.exports = {
               // base for generating different widths of each image.
               maxWidth: 2000,
             },
-          }
-        ]
-      }
-    },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-sass',
-      options: {
-        includePaths: [`${__dirname}/node_modules`, `${__dirname}/src/`],
-        precision: 8
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-google-fonts',
-      options: {
-        fonts: [
-          'material-icons',
-          'Montserrat:400',
-        ]
-      }
-    },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-          name: 'Cristina Soler',
-          short_name: 'Cristina Soler',
-          start_url: '/',
-          background_color: '#EFFFE9',
-          theme_color: '#2EC4B6',
-          icon: 'static/files/me.jpeg',
+          },
+        ],
       },
-    }
-  ]
-}
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        sassOptions: {
+          includePaths: [`${__dirname}/node_modules`, `${__dirname}/src/`],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Montserrat\:400`],
+        display: "swap",
+      },
+    },
+    "gatsby-plugin-netlify", // make sure to keep it last in the array
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Cristina Soler",
+        short_name: "Cristina Soler",
+        start_url: "/",
+        background_color: "#EFFFE9",
+        theme_color: "#2EC4B6",
+        icon: "static/files/me.jpeg",
+      },
+    },
+  ],
+};
