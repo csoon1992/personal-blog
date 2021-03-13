ce72e11b378615861b7f4a4d39658ea8224486f8
Cristina Soler <csoon1992@gmail.com>
2021-03-13 17:49:58 +0100

    Remove unused files

--- src/components/Blog/Md/Code.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Code = ({children}) => (
-    <code className="mdc code p-1">
-        {children}
-    </code>
-);
-
-export default Code;
--- src/components/Blog/Md/Link.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Link = ({children, href}) => (
-    <a href={href} target="_blank" rel="noopener noreferrer" className="mdc link">
-        {children}
-    </a>
-);
-
-export default Link;
